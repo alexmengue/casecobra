524cebccebdb0a70ff5653309a4454045661293d
Alex <alexmengue1990@hotmail.com>
2024-06-13 22:34:41 -0300

    new component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-slate-50"></div>
+    <div className="bg-slate-50">
+      <section>
+        <MaxWidthWrapper></MaxWidthWrapper>
+      </section>
+    </div>
   );
 }
