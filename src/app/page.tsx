5bc416b3d1c0037986e1e08adeff938e2ba43fdc
Alex <alexmengue1990@hotmail.com>
2024-06-15 16:58:31 -0300

    started a new section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,15 @@ export default function Home() {
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
                 <img src="/snake-1.png" className="w-full" />
               </div>
+
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5x1 md:text-6x1 lg:text-7x1">
+                Your Image on a <span className="bg-green-600 px-2 text-white">Custom</span> Phone Case
+              </h1>
+
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                Capture your favorite memories with your own, {''} <span className="font-semibold">one-of-one</span> phone case.
+                CaseCobra allows you do protect your memories, not just your phone case.
+              </p>
             </div>
           </div>
         </MaxWidthWrapper>
