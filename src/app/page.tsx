ddfbbcf35a09feb85d1ea59fd7ebae516c2a3320
Alex <alexmengue1990@hotmail.com>
2024-06-17 19:43:03 -0300

    added users section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,6 +40,22 @@ export default function Home() {
                   </li>
                 </div>
               </ul>
+              
+              <div className="mt-12 flex flex-col sm:flex-row  items-center sm:items-start gap-5">
+                <div className="flex -space-x-4">
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-1000" src="/users/user-1.png" alt="user image" />
+
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-1000" src="/users/user-2.png" alt="user image" />
+
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-1000" src="/users/user-3.png" alt="user image" />
+
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-1000" src="/users/user-4.jpg" alt="user image" />
+
+                  <img className="inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-1000" src="/users/user-5.jpg" alt="user image" />
+                </div>
+
+                
+              </div>
             </div>
           </div>
         </MaxWidthWrapper>
