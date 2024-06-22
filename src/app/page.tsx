87d5d88ae6f42dcf9a95345789aeae30be9c95c8
Alex <alexmengue1990@hotmail.com>
2024-06-21 23:56:07 -0300

    new img added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,6 +68,8 @@ export default function Home() {
           <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
             <div className="relative md:max-w-x1">
               <img className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block" src="/your-image.png" alt="your image" />
+
+              <img className="absolute w-20 -left-6 -bottom-6 select-none" src="/line.png" alt="" />
             </div>
           </div>
         </MaxWidthWrapper>
