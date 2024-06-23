b1fa6d5700c82fd4e83e2e88fd58f004388f4c07
Alex <alexmengue1990@hotmail.com>
2024-06-23 20:36:09 -0300

    update Phone component

--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -1,6 +1,12 @@
 import { cn } from "@/lib/utils";
+import { HTMLAttributes } from "react";
 
-const Phone = ({className}: {}) => {
+interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
+  imgSrc: string
+  dark?: boolean
+}
+
+const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
   return (<div className={cn("relative pointer-events-none z-50 overflow-hidden", className)}></div>)
 }
 
