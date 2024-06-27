61a7417a67c989abf3a8f5bf377c3f2d4536e269
Alex <alexmengue1990@hotmail.com>
2024-06-26 22:25:59 -0300

    new component navbar added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import MaxWidthWrapper from "./MaxWidthWrapper";
+
+const Navbar = () => {
+  <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+    
+  </nav>
+}
+
+export default Navbar;
