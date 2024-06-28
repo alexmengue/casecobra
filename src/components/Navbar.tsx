7ef7c889c61126ce1be0a765ed5ac314ce0858c1
Alex <alexmengue1990@hotmail.com>
2024-06-28 20:17:46 -0300

    navbar added to the layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from "next/link";
 const Navbar = () => {
-  <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+ return <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
     <MaxWidthWrapper>
       <div className="flex h-4 items-center justify-between border-b border-zinc-200">
         <Link className="flex z-40 font-semibold" href="/">
